60bd132cf08e8b47248e89f55bcce2b7bb656d87
Jabar <violetta.je.vymmiy15@gmail.com>
2023-09-07 09:28:06 +0700

    Minggu 4 soal prioritas 2 soal platform dan widget layout

---
+++ Minggu 4/2_Soal Platform Widget dan Layout/praktikum/Soal Prioritas 2/cupertino_design/lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoApp(
+      title: 'Flutter Demo',
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoApp(
+      title: 'CupertinoApp',
+      home: FirstPage(),
+      debugShowCheckedModeBanner: false,
+      theme : themeDark
+    );
+  }
+}
+
+final themeDark = CupertinoThemeData.raw(
+  Brightness.dark,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+);
+
+class FirstPage extends StatefulWidget {
+  const FirstPage({Key? key}) : super(key: key);
+
+  @override
+  State<FirstPage> createState() => _FirstPageState();
+}
+
+class _FirstPageState extends State<FirstPage> {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: Text('CupertinoApp'),
+      ),
+      child: ListView(
+
+      ),
+
+    );
+  }
+}
+
+
